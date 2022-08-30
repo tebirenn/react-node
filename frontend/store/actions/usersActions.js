022c1f8ead0aee4d30b6873829e2ebe98d678be1
Alan <a_saudabekov@kbtu.kz>
2022-08-30 11:53:49 +0600

    first

--- frontend/store/actions/usersActions.js
+++
@@ -1,5 +0,0 @@
-import * as types from './types';
-
-export function registerUser (data) {
-    return {type: types.REGISTER_USER, data};
-}
--- frontend/utils/consts.js
+++
@@ -1,4 +0,0 @@
-export const USER_ROUTE = '/user';
-export const LOGIN_ROUTE = '/login';
-export const REGISTRATION_ROUTE = '/registration';
-export const BLOG_ROUTE = '/blog';
